ff8893a842ca83d0bbda533c577f541b55807d45
mgracnazareno <mgracenazareno@gmail.com>
2023-12-08 14:19:57 -0500

    ajout class notification

--- src/main/java/com/example/agendadesevenements/entity/Administrateur.java
+++ src/main/java/com/example/agendadesevenements/entity/Administrateur.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 
 import java.util.List;
 
-
 @Entity
 @Table(name="administrateur")
 public class Administrateur {
---
+++ src/main/java/com/example/agendadesevenements/entity/Notification.java
@@ -0,0 +1,22 @@
+package com.example.agendadesevenements.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="notification")
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idNotification;
+
+    @ManyToOne
+    @JoinColumn(name="idEvenement")
+    private Evenement evenement;
+
+    @ManyToOne
+    @JoinColumn(name="idUtilisateur")
+    private Utilisateur utilisateur;
+
+
+}
