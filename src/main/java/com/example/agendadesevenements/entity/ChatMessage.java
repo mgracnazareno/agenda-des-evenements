9bb923e48381313a1f65722f08cd5b51ae9a35df
mgracnazareno <mgracenazareno@gmail.com>
2023-12-08 14:24:35 -0500

    ajout classe chatmessage

---
+++ src/main/java/com/example/agendadesevenements/entity/ChatMessage.java
@@ -0,0 +1,25 @@
+package com.example.agendadesevenements.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="chatmessage")
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idChat;
+
+    @Column(name="message", length = 255)
+    private String message;
+
+    @Column(name="dateMessage")
+    private Date dateMessage;
+
+    @ManyToOne
+    @JoinColumn(name="idUtilisateur")
+    private Utilisateur utilisateur;
+
+
+}
