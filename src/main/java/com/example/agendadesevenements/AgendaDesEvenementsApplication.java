6b86e7d91ac78b7c46252047b58c90ef48f8d07a
mgracnazareno <mgracenazareno@gmail.com>
2023-12-08 09:43:49 -0500

    initial commit

---
+++ src/main/java/com/example/agendadesevenements/AgendaDesEvenementsApplication.java
@@ -0,0 +1,13 @@
+package com.example.agendadesevenements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDesEvenementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaDesEvenementsApplication.class, args);
+    }
+
+}
